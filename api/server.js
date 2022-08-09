1a7a719afdc606882ba7819bef4621a947bc78ec
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-08-09 03:01:00 +0200

    work in progress, created get end points

--- api/server.js
+++ api/server.js
@@ -1,3 +1,38 @@
-// BUILD YOUR SERVER HERE
+// IMPORTS
+const express = require("express");
+const Users = require("../api/users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// INSTANCE OF EXPRESS APP
+const server = express();
+
+// GLOBAL MIDDLEWARE
+server.use(express.json());
+
+// -------------------ENDPOINTS--------------------
+
+//GET all
+server.get("/api/users", (req, res) => {
+  Users.find()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "The users information could not be retrieved" });
+    });
+});
+
+//GET BY ID
+server.get("/api/users/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: "The user with the specified ID does not exist" });
+      } else res.json(data);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "The users information could not be retrieved" });
+    });
+});
+
+// -------------------EXPORTS
+module.exports = server;
