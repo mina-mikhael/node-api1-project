6275149af301b57c418c450af166f9e03bcb4077
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-08-09 13:34:52 +0200

    mvp

--- api/server.js
+++ api/server.js
@@ -13,8 +13,8 @@ server.use(express.json());
 //GET all
 server.get("/api/users", (req, res) => {
   Users.find()
-    .then((data) => {
-      res.json(data);
+    .then((users) => {
+      res.json(users);
     })
     .catch(() => {
       res.status(500).json({ message: "The users information could not be retrieved" });
@@ -24,14 +24,60 @@ server.get("/api/users", (req, res) => {
 //GET BY ID
 server.get("/api/users/:id", (req, res) => {
   Users.findById(req.params.id)
-    .then((data) => {
-      if (!data) {
+    .then((user) => {
+      if (!user) {
         res.status(404).json({ message: "The user with the specified ID does not exist" });
-      } else res.json(data);
+      } else res.json(user);
     })
     .catch(() => {
       res.status(500).json({ message: "The users information could not be retrieved" });
     });
+});
+
+//POST A NEW USER
+server.post("/api/users", (req, res) => {
+  Users.insert(req.body)
+    .then((newUser) => {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" });
+      } else res.status(201).json(newUser);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "The users information could not be retrieved" });
+    });
+});
+
+//DELETE A USER
+server.delete("/api/users/:id", (req, res) => {
+  Users.remove(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ message: "The user with the specified ID does not exist" });
+      } else res.json(user);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "The user could not be removed" });
+    });
+});
+
+//Change a user
+server.put("/api/users/:id", (req, res) => {
+  Users.update(req.params.id, req.body)
+    .then((result) => {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" });
+      } else if (!result) {
+        res.status(404).json({ message: "The user with the specified ID does not exist" });
+      } else res.json(result);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "The user information could not be modified" });
+    });
+});
+
+//CATCH ALL
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "not Found , lool" });
 });
 
 // -------------------EXPORTS
